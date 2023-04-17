95bd6c5164e6505478ecbacd271b65430e8d9bcb
Elinelin22 <elin.eckervald.4418@student.uu.se>
2023-04-17 09:48:42 +0200

    test skicka jobb från serversidan

--- assets/screens/LikeScreen.js
+++ assets/screens/LikeScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {View, TextInput, Text, ScrollView} from "react-native";
+import {View, TextInput, Text, ScrollView, TouchableOpacity} from "react-native";
 import JobCard from "../components/JobCard";
 import SEB from "../images/SEB.jpeg";
 import sweco from "../images/sweco.png";
@@ -13,9 +13,27 @@ const LikeScreen = () => {
         setSearchText(text);
     };
 
+    const [serverResponse, setServerResponse] = useState("");
+
+    const fetchJobs = async () => {
+        try {
+            const response = await fetch("http://127.0.0.1:8000/api/fetchJobs");
+            const data = await response.json();
+            console.log(data);
+            setServerResponse(data.message); // set the server response as state
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
     return (
         <ScrollView>
             <View className="flex-1">
+                <TouchableOpacity onPress={fetchJobs}>
+                    <Text>{serverResponse}</Text>
+                    <Text> Get jobs</Text>
+                </TouchableOpacity>
                 <View className="mb-3">
                     <TextInput
                         className="h-10 border border-gray-300 rounded px-3"
