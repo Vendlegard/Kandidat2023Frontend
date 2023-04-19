b7970cdda4d103df17b19609ede0ba49fa51c2ed
daniel0222u <daniel.ceoca.0222@student.uu.se>
2023-04-19 15:08:44 +0200

    En fil kom inte med. Sorry.

---
+++ assets/screens/CompetenceScreen.js
@@ -0,0 +1,31 @@
+/*[
+    {
+        questionIndex: 0,
+        studyLocation: "Uppsala",
+        studyProgram: "Computer Science",
+        studyPeriod: "2018-2022"
+    },
+    {
+        questionIndex: 1,
+        lookingForPreferences: ["Internship", "Part-time job"],
+    },
+    {
+        questionIndex: 2,
+        competencies: ["C#", "Java", "Python"]
+    }
+] */
+
+import React, {useState} from "react";
+import {View, Text, TextInput, Button, TouchableOpacity}  from "react-native";
+
+//create a basic component
+const CompetenceScreen = () => {
+
+    return (
+        <View>
+            <Text>CompetenceScreen</Text>
+        </View>
+    );
+
+}
+export default CompetenceScreen;
