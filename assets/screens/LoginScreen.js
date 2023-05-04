1d5c32821f1f69b6f14bb4e9f2b6cc808b784622
victoriaberinder <victoria.berinder@gmail.com>
2023-05-04 10:50:10 +0200

    Startsida, login och signup

--- assets/screens/LoginScreen.js
+++ assets/screens/LoginScreen.js
@@ -121,56 +121,36 @@ const LoginScreen = ({ updateLoggedInState, updateRegisterState, updateUserInfo 
 
         <View className="flex-1 justify-center items-center">
 
-             {/* Profile Bubbles */}
-                <View className="absolute top-0 right-0">
-            {/* ProfileScreen Bubble */}
-                    <View className="w-28 h-28 bg-profileScreen opacity-70 rounded-full mt-12 mr-8"></View>
-
-            {/* Pink Bubble */}
-                    
-
-            {/* Gray Bubble */}
-                </View>
-                <View className="absolute bottom-0 left-0">
-                    <View className="w-20 h-20 bg-profileScreen opacity-70 rounded-full -mb-5 ml-2"></View>
-                    <View className="w-14 h-14 bg-purple opacity-90 rounded-full mb-20 ml-12"></View>
-                    </View>
+            
             <Text className = 'text-4xl m-4'> Please sign in with {userData.firstName}</Text>
 
             
             <TextInput
-                style = {{fontSize: 22, width: "75%", height: "5%", margin: "5%", borderRadius: 12,  backgroundColor: "#E6E6FA", 
-                shadowOffset: {width:0, height: 2}, shadowColor: '#000', shadowOpacity: 0.25, shadowRadius: 3, elevation: 2}}
+                style = {{fontSize: 22, width: "75%", height: "5%", margin: "5%", borderRadius: 10,  backgroundColor: "#e3f1ff", paddingHorizontal: 6,
+                shadowOffset: {width:0, height: 1}, shadowColor: '#000', shadowOpacity: 0.15, shadowRadius: 3, elevation: 2}}
                 placeholder="Email"
                 onChangeText={onChangeEmailAdressAuth}
                 value={emailAdressAuth}
             />
             <TextInput 
-                style = {{fontSize: 22 ,width: "75%", height: "5%", margin: "5%", borderRadius: 12,  backgroundColor: "#E6E6FA", 
-                shadowOffset: {width:0, height: 2}, shadowColor: '#000', shadowOpacity: 0.25, shadowRadius: 3, elevation: 2}}
+                style = {{fontSize: 22 ,width: "75%", height: "5%", margin: "5%", borderRadius: 10,  backgroundColor: "#e3f1ff", paddingHorizontal: 6,
+                shadowOffset: {width:0, height: 1}, shadowColor: '#000', shadowOpacity: 0.15, shadowRadius: 3, elevation: 2}}
                 placeholder="Password"
                 onChangeText={onChangePasswordAuth}
                 value={passwordAuth}
                 secureTextEntry={true}
             />
 
-            {/* */}
-            <View>
-                <TouchableOpacity className= "m-4" onPress={() => forgotPassword()}>
-                    <Text style={{fontSize: 20, color: '#A9A9A9'}} > Forgot password?</Text>
-                </TouchableOpacity>
-            </View>
-
-            <View >
-                    <TouchableOpacity className="bg-black w-20 h-9 justify-center items-center rounded-xl mt-5 mb-10" onPress={() => handleLogin(emailAdressAuth,passwordAuth)}>
-                        <Text className="text-white text-sm">LOGIN</Text>
+            <View className="mt-10" >
+                    <TouchableOpacity style={{backgroundColor: 'black', width: 130, height:40, borderWidth:1, borderColor:'808080'}} className="justify-center items-center rounded-xl" onPress={() => handleLogin(emailAdressAuth,passwordAuth)}>
+                        <Text style={{color: 'white'}}>LOGIN</Text>
                     </TouchableOpacity>
             </View>
 
             <View className="items-center -mb-10">
-                <Text style={{fontSize: 20, color: '#A9A9A9', margin: '5%'}} > Don't have an account yet? </Text>
-                <TouchableOpacity className="bg-black w-20 h-9 justify-center items-center rounded-xl mt-2"onPress={() => handleRegister()}>
-                    <Text className="text-white text-sm">SIGNUP</Text>
+                <Text style={{fontSize: 20, color: '#A9A9A9', marginTop:40, marginBottom:5}} > Don't have an account yet? </Text>
+                <TouchableOpacity style={{backgroundColor: 'black', width: 130, height:40, borderWidth:1, borderColor:'808080'}} className="justify-center items-center rounded-xl mt-2"onPress={() => handleRegister()}>
+                    <Text style={{color: 'white'}}>SIGN UP</Text>
                 </TouchableOpacity>
             </View>
 
