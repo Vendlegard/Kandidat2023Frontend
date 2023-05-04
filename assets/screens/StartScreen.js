1ccb26e4056520298ce9f245a90f03f943a988e5
Vendlegard <emil.vendlegard@gmail.com>
2023-05-04 09:11:34 +0200

    Styling startScreen

--- assets/screens/StartScreen.js
+++ assets/screens/StartScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, Image, Button} from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Image, Button, StyleSheet} from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import peaceSign from '../images/peaceSign.png'
 
@@ -19,66 +19,67 @@ const StartScreen = ({goToLogin, goToCreate}) => {
 
 
     return (
-        <View className="flex-1 bg-white">
+    <View style = {styles.container}>
+        <View style = {styles.logoContainer}>
+            <Text className="font-railway text-5xl font-bold mr-2">Swipe</Text>
+            <Image source={peaceSign} className="w-11 h-16 items-center"></Image>
+            <Text className="font-railway text-5xl font-bold ml-2">Work</Text>
+        </View>  
 
-        <View className="absolute top-0 left-0">
-                            {/* ProfileScreen Bubble */}
-                            <View className="w-28 h-28 bg-profileScreen rounded-full -mt-3 -ml-2"></View>
+        <View style = {styles.buttonContainer}>
+            <TouchableOpacity style = {styles.button}
+                onPress={() => passGoToLoginToApp(false)}
+                >
+                <Text style = {styles.buttonText}>Logga in</Text>
+            </TouchableOpacity>        
 
-                            {/* Pink Bubble */}
-                            <View className="w-20 h-20 bg-purple rounded-full -mt-8 ml-8"></View>
+            <TouchableOpacity style = {styles.button}
+            onPress={() => passGoToCreateToApp(true) }
+            >
+                    <Text style = {styles.buttonText}>Skapa konto</Text>
+            </TouchableOpacity>
 
-                            {/* Gray Bubble */}
-                            <View className="absolute top-0 left-full mt-8 ml-2">
-                                <View className="w-14 h-14 bg-lightgray rounded-full -ml-10"></View>
-                            </View>
-                        </View>
 
-        <View className= "flex-1 flex-row justify-center mt-12">
-            
-        <Text className="font-railway text-4xl font-bold mr-2 mt-7">Swipe</Text>
-        <Image source={peaceSign} className="w-11 h-16 mt-4"></Image>
-        <Text className="font-railway text-4xl font-bold ml-2 mt-7">Work</Text>                  
         </View>
-
-
-        <View className="flex-1 flex-col justify-center items-center mb-20">
-                        <TouchableOpacity className="bg-black w-20 h-9 flex justify-center
-                         items-center rounded m-10" 
-                         onPress={() => passGoToLoginToApp(false)}
-                         >
-                            <Text className="text-white text-sm">Logga in</Text>
-                        </TouchableOpacity>
-                       
-
-
-                    <TouchableOpacity className="bg-black w-20 h-9 flex 
-                    justify-center items-center rounded"
-                    onPress={() => passGoToCreateToApp(true) }
-                    >
-                            <Text className="text-white text-sm">Skapa konto</Text>
-                        </TouchableOpacity>
-
-
-                        </View>
-
-        <View className="absolute bottom-10 right-2">
-                            {/* ProfileScreen Bubble */}
-                            <View className="w-28 h-28 bg-profileScreen rounded-full -mt-3 -ml-2"></View>
-
-                            {/* Pink Bubble */}
-                            <View className="w-20 h-20 bg-purple rounded-full -mt-8 ml-8"></View>
-
-                            {/* Gray Bubble */}
-                            <View className="absolute top-0 left-full mt-8 ml-2">
-                                <View className="w-14 h-14 bg-lightgray rounded-full -ml-10"></View>
-                            </View>
-                        </View>
  
 
     </View>
 
     );
 };
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#E6E6FA',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    logoContainer:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingBottom: 250
+    },
+    buttonContainer: {
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        paddingTop: 60,
+        marginBottom: 20
+      },
+      button: {
+        backgroundColor: 'black',
+        borderRadius: 5,
+        padding: 10,
+        margin: 10,
+      },
+      buttonText: {
+        color: 'white',
+        fontSize: 16,
+      },
+      bubbleTop:{
+        justifyContent: "flex-start",
+        alignItems: 'left',
+      }
+})
 
 export default StartScreen;
